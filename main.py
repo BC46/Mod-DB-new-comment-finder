9b8901b5ea4129dde9c4d61291ab8c0b124206c5
C.S. Melis <cees3331x00@gmail.com>
2022-09-04 11:29:23 +0200

    Clean up script

--- main.py
+++ main.py
@@ -3,18 +3,14 @@
 # Go to all pages
 # Paginate to other pages in same page utnil span.current is not equal to current page anymore
 
-
-
 import requests
 from bs4 import BeautifulSoup
 import datetime
 import re
 
-recent_post_regex = re.compile('^\d+ ?(secs?|mins?) ago$')
-
-recent_comments = []
-
 class Comment:
+    recent_post_regex = re.compile('^\d+ ?(secs?|mins?) ago$')
+
     def __init__(self, author, content, date_time, date_time_text, page_url, page_number):
         self.author = author
         self.content = content
@@ -24,7 +20,7 @@ class Comment:
         self.page_number = page_number
         
     def is_recent(self):
-        return bool(recent_post_regex.match(self.date_time_text))
+        return bool(self.recent_post_regex.match(self.date_time_text))
         
     def equals(self, other):
         return self.author == other.author and self.content == other.content and self.date_time == other.date_time and self.page_url == other.page_url
@@ -35,34 +31,42 @@ class Comment:
         
         return f"New comment from {self.author} posted {time_diff_min} {minute_word} ago:\n\n" + self.content + f"\n\nReply here: {self.page_url}/page/{self.page_number}"
 
-url = "https://www.moddb.com/mods/freelancer-hd-edition"
-#url = "https://www.moddb.com/mods/freelancer-hd-edition/downloads/freelancer-hd-edition-v06"
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content, "html.parser")
+def start():
+    recent_comments = []
 
-comments_container = soup.find('div', class_='table tablecomments')
+    url = "https://www.moddb.com/mods/freelancer-hd-edition"
+    #url = "https://www.moddb.com/mods/freelancer-hd-edition/downloads/freelancer-hd-edition-v06"
+    page = requests.get(url)
 
-for comment in comments_container.find_all('div', class_='content'):
-    author = comment.find('a', class_='author').text
-    content = comment.find('p').text.strip()
-    
-    time_element = comment.find('time')
-    
-    date_time = time_element['datetime']
-    date_time = date_time[:len(date_time) - 6]
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    date_time_obj = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
-    
-    # TODO: It's possible that this doesn't work (no pages)
-    page_number = int(soup.find('span', class_='current').text)
-    
-    new_comment = Comment(author, content, date_time_obj, time_element.text, url, page_number)
-    
-    if new_comment.is_recent():
-        comments.append(new_comment)
-    
-    print(new_comment)
-    print(page_number)
-    print(time_element.text)
-    print(new_comment.is_recent())
+    comments_container = soup.find('div', class_='table tablecomments')
+
+    for comment in comments_container.find_all('div', class_='content'):
+        author = comment.find('a', class_='author').text
+        content = comment.find('p').text.strip()
+        
+        time_element = comment.find('time')
+        
+        date_time = time_element['datetime']
+        date_time = date_time[:len(date_time) - 6]
+
+        date_time_obj = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+        
+        # TODO: It's possible that this doesn't work (no pages)
+        page_number = int(soup.find('span', class_='current').text)
+        
+        new_comment = Comment(author, content, date_time_obj, time_element.text, url, page_number)
+        
+        if new_comment.is_recent():
+            comments.append(new_comment)
+        
+        print(new_comment)
+        print(page_number)
+        print(time_element.text)
+        print(new_comment.is_recent())
+
+
+if __name__ == "__main__":
+    start()
